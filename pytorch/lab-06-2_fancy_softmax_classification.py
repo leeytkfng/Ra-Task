2a07788882f282f6c0851b02ff83e551ed86b54a
iyongsu <dydtn61498@naver.com>
2026-09-20 00:41:34 +0900

    lee

--- pytorch/lab-06-2_fancy_softmax_classification.py
+++ pytorch/lab-06-2_fancy_softmax_classification.py
@@ -4,11 +4,35 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 
 
-torch.manual_seed(777)
+torch.manual_seed(1)
 
 data_path = Path(__file__).with_name('data-04-zoo.csv')
+
+z = torch.rand(3,5 , required_grad=True)
+hypothesis = F.softmax(z,dim=1)
+y = torch.randint(5,(3,)).long()
+y_one_hot = torch.zeros_like(hypothesis)
+y_one_hot.scatter_(1, y.unsqueeze(1),1)
+
+# Low Level
+torch.log(F.softmax(z, dim=1))
+
+# High level
+F.log_softmax(z,dim=1)
+
+# Low level
+(y_one_hot * -torch.log(F.softmax(z,dim=1))).sum(dim=1).mean()
+
+# High level 
+F.nll_loss(F.log_softmax(z,dim=1), y.long())
+
+F.cross_entropy(z,y)
+
+
+
 
 # TODO 1: np.loadtxt()로 Zoo CSV 데이터를 불러오세요.
 
